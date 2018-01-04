1b382d106b054aa67c2c8448b126080a7653762b
rameshnadimpalli <rkraju.n@gmail.com>
2018-01-04 13:10:26 +0530

    first commit

---
+++ second/src/main/java/com/nic/in/second/App.java
@@ -0,0 +1,13 @@
+package com.nic.in.second;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
